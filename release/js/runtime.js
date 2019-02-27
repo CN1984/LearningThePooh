26030ada89250233ab916f63342e7d41e995fb52
CN1984 <47753275+CN1984@users.noreply.github.com>
2019-02-27 16:11:13 +0800

    v1.2.2
    fixed page refresh bug

--- release/js/runtime.js
+++ release/js/runtime.js
@@ -1,9 +1,10 @@
 chrome.runtime.sendMessage({"method": "checkTab"}, {}, function (response) {
     if (response.hasOwnProperty("runtime")) {
         if (response.runtime === 1) {
+            window.scrollTo(0, 0);
             setInterval(function () {
                 let scrollYMax = document.body.scrollHeight - document.body.clientHeight;
-                let scrollY = window.scrollY + Math.floor(Math.random() * 100) + 1;
+                let scrollY = window.scrollY + Math.floor(Math.random() * 20) + 1;
                 window.scrollTo(window.scrollX, scrollY < scrollYMax ? scrollY : scrollYMax);
 
                 let playBtn = document.querySelector(".prism-big-play-btn");
